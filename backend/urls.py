c8eab36795c373ac4fe476db436cc1a65b4bf55d
Javid Ahmad Rashid <javidahmadrashid.com>
2025-08-01 22:56:55 +0430

    New Project Started

--- backend/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include # Import include to include other URL configurations
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/users/', include('users.urls')),
-    # other paths...
-]
