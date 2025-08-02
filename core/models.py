a33a27d484234ae7f8ec6d5376df21fed8c49b20
Javid Ahmad Rashid <javidahmadrashid.com>
2025-08-02 20:18:34 +0430

    User api test and implementaion added to the project

---
+++ core/models.py
@@ -0,0 +1,47 @@
+"""
+Database Models for the application.
+"""
+
+from django.db import models
+from django.contrib.auth.models import(
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
+
+class UserManager(BaseUserManager):
+    """Manager for users"""
+    
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return a user with an email and password"""
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(email = self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        """"Create and return a superuser with an email and password"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+
+        user.save(using = self._db)
+
+        return user
+    
+    
+    
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the System"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    # password = models.CharField(max_length=128, null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)   
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
